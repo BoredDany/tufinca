44a6fd3c9582d9ba1505313a984068551188d93a
BoredDany <martinezq.d@javeriana.edu.co>
2024-03-05 16:23:14 -0500

    First

---
+++ src/test/java/web/rent/tufinca/TufincaApplicationTests.java
@@ -0,0 +1,13 @@
+package web.rent.tufinca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TufincaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

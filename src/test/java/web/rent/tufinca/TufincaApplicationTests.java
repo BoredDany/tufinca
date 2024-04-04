523b6bbe76b42299019516a1f5b48a91166c633c
BoredDany <martinezq.d@javeriana.edu.co>
2024-04-04 02:26:20 -0500

    sonar properties in pom.xml

--- src/test/java/web/rent/tufinca/TufincaApplicationTests.java
+++ src/test/java/web/rent/tufinca/TufincaApplicationTests.java
@@ -2,12 +2,15 @@ package web.rent.tufinca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class TufincaApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
+
+	
 
 }

03855732475f0558b6d1c660a834f565c8cd8046
BoredDany <martinezq.d@javeriana.edu.co>
2024-04-17 23:40:31 -0500

    Competo, falta manejo de excepciones

--- src/main/java/web/rent/tufinca/mappers/ModelMapperConfiguration.java
+++ src/main/java/web/rent/tufinca/mappers/ModelMapperConfiguration.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,90 +18,174 @@ import web.rent.tufinca.entities.Property;
 import web.rent.tufinca.entities.Rent;
 import web.rent.tufinca.entities.RentRequest;
 import web.rent.tufinca.entities.User;
+import web.rent.tufinca.repositories.RepositoryPhoto;
+import web.rent.tufinca.repositories.RepositoryProperty;
+import web.rent.tufinca.repositories.RepositoryRent;
+import web.rent.tufinca.repositories.RepositoryRentRequest;
+import web.rent.tufinca.repositories.RepositoryUser;
+
+import java.util.List;
 
 @Configuration
 public class ModelMapperConfiguration {
+
+    @Autowired
+    private RepositoryProperty propertyRepository;
+
+    @Autowired
+    private RepositoryRent rentRepository;
+
+    @Autowired
+    private RepositoryRentRequest rentRequestRepository;
+
+    @Autowired
+    private RepositoryUser userRepository;
+
+    @Autowired
+    private RepositoryPhoto photoRepository;
+    
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         
-        modelMapper.typeMap(User.class, UserDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getProperties() != null ? src.getProperties().stream().map(Property::getIdProperty).collect(Collectors.toList()) : null, UserDTO::setPropertyIds);
-            mapper.map(src -> src.getReservations() != null ? src.getReservations().stream().map(Rent::getIdRent).collect(Collectors.toList()) : null, UserDTO::setReservationIds);
-            mapper.map(src -> src.getRents() != null ? src.getRents().stream().map(Rent::getIdRent).collect(Collectors.toList()) : null, UserDTO::setRentIds);
-            mapper.map(src -> src.getRentRequests() != null ? src.getRentRequests().stream().map(RentRequest::getIdRentRequest).collect(Collectors.toList()) : null, UserDTO::setRentRequestIds);
-            mapper.map(src -> src.getReservationRequests() != null ? src.getReservationRequests().stream().map(RentRequest::getIdRentRequest).collect(Collectors.toList()) : null, UserDTO::setReservationRequestIds);
-        });
-        
-        // Mapping for Property to PropertyDTO
-        modelMapper.typeMap(Property.class, PropertyDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getUser() == null ? null : src.getUser().getIdUser(), PropertyDTO::setOwnerId);
-            mapper.map(src -> src.getRents() == null ? null : src.getRents().stream().map(Rent::getIdRent).collect(Collectors.toList()), PropertyDTO::setRentIds);
-            mapper.map(src -> src.getRentRequests() == null ? null : src.getRentRequests().stream().map(RentRequest::getIdRentRequest).collect(Collectors.toList()), PropertyDTO::setRentRequestIds);
-            mapper.map(src -> src.getPhotos() == null ? null : src.getPhotos().stream().map(Photo::getIdPhoto).collect(Collectors.toList()), PropertyDTO::setPhotoIds);
-        });
 
-
-        // Mapping for Rent to RentDTO
-        modelMapper.typeMap(Rent.class, RentDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getOwner() == null ? null : src.getOwner().getIdUser(), RentDTO::setOwnerId);
-            mapper.map(src -> src.getRenter() == null ? null : src.getRenter().getIdUser(), RentDTO::setRenterId);
-            mapper.map(src -> src.getProperty() == null ? null : src.getProperty().getIdProperty(), RentDTO::setPropertyId);
-        });
-
-        // Configuración para mapear el ID del propietario, inquilino y propiedad en RentRequest a RentRequestDTO
-        modelMapper.typeMap(RentRequest.class, RentRequestDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getOwner() == null ? null : src.getOwner().getIdUser(), RentRequestDTO::setOwnerId);
-            mapper.map(src -> src.getRenter() == null ? null : src.getRenter().getIdUser(), RentRequestDTO::setRenterId);
-            mapper.map(src -> src.getProperty() == null ? null : src.getProperty().getIdProperty(), RentRequestDTO::setPropertyId);
-        });
-        
-        // Configuración para mapear el ID de la propiedad en Photo a PhotoDTO
-        modelMapper.typeMap(Photo.class, PhotoDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getProperty() == null ? null : src.getProperty().getIdProperty(), PhotoDTO::setPropertyId);
-        });
-
-
-        //DTO  A ENTIDAD
-
-
-        // Mapping for UserDTO to User
+        //Mapping for UserDTO to User
         modelMapper.typeMap(UserDTO.class, User.class).addMappings(mapper -> {
-            mapper.skip(User::setProperties);
-            mapper.skip(User::setReservations);
-            mapper.skip(User::setRents);
-            mapper.skip(User::setRentRequests);
-            mapper.skip(User::setReservationRequests);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> propertyRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(UserDTO::getPropertyIds, User::setProperties);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> rentRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(UserDTO::getRentIds, User::setRents);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> rentRequestRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(UserDTO::getRentRequestIds, User::setRentRequests);
         });
 
-        // Mapping for PropertyDTO to Property
+        //Mapping for User to UserDTO
+        modelMapper.typeMap(User.class, UserDTO.class).addMappings(mapper -> {
+            mapper.using(ctx -> ((List<Property>) ctx.getSource()).stream()
+                .map(Property::getIdProperty)
+                .collect(Collectors.toList()))
+                .map(User::getProperties, UserDTO::setPropertyIds);
+            
+            mapper.using(ctx -> ((List<Rent>) ctx.getSource()).stream()
+                .map(Rent::getIdRent)
+                .collect(Collectors.toList()))
+                .map(User::getRents, UserDTO::setRentIds);
+
+            mapper.using(ctx -> ((List<Rent>) ctx.getSource()).stream()
+                .map(Rent::getIdRent)
+                .collect(Collectors.toList()))
+                .map(User::getReservations, UserDTO::setReservationIds);
+            
+            mapper.using(ctx -> ((List<RentRequest>) ctx.getSource()).stream()
+                .map(RentRequest::getIdRentRequest)
+                .collect(Collectors.toList()))
+                .map(User::getRentRequests, UserDTO::setRentRequestIds);
+
+            mapper.using(ctx -> ((List<RentRequest>) ctx.getSource()).stream()
+                .map(RentRequest::getIdRentRequest)
+                .collect(Collectors.toList()))
+                .map(User::getReservationRequests, UserDTO::setReservationRequestIds);
+        });
+
+        //Mapping for PropertyDTO to Property
         modelMapper.typeMap(PropertyDTO.class, Property.class).addMappings(mapper -> {
-            mapper.skip(Property::setUser);
-            mapper.skip(Property::setRents);
-            mapper.skip(Property::setRentRequests);
-            mapper.skip(Property::setPhotos);
+            mapper.using(ctx -> userRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(PropertyDTO::getOwnerId, Property::setUser);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> rentRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(PropertyDTO::getRentIds, Property::setRents);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> rentRequestRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(PropertyDTO::getRentRequestIds, Property::setRentRequests);
+            mapper.using(ctx -> {
+                List<Long> source = (List<Long>) ctx.getSource();
+                return source.stream()
+                    .map(id -> photoRepository.findById(id).orElse(null))
+                    .collect(Collectors.toList());
+            }).map(PropertyDTO::getPhotoIds, Property::setPhotos);
         });
 
-        // Mapping for RentDTO to Rent
-        modelMapper.typeMap(RentDTO.class, Rent.class).addMappings(mapper -> {
-            mapper.skip(Rent::setOwner);
-            mapper.skip(Rent::setRenter);
-            mapper.skip(Rent::setProperty);
+        //Mapping for Property to PropertyDTO
+        modelMapper.typeMap(Property.class, PropertyDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getUser().getIdUser(), PropertyDTO::setOwnerId);
+            mapper.using(ctx -> ((List<Rent>) ctx.getSource()).stream()
+                .map(Rent::getIdRent)
+                .collect(Collectors.toList()))
+                .map(Property::getRents, PropertyDTO::setRentIds);
+            mapper.using(ctx -> ((List<RentRequest>) ctx.getSource()).stream()
+                .map(RentRequest::getIdRentRequest)
+                .collect(Collectors.toList()))
+                .map(Property::getRentRequests, PropertyDTO::setRentRequestIds);
+            mapper.using(ctx -> ((List<Photo>) ctx.getSource()).stream()
+                .map(Photo::getIdPhoto)
+                .collect(Collectors.toList()))
+                .map(Property::getPhotos, PropertyDTO::setPhotoIds);
         });
 
-        // Mapping for RentRequestDTO to RentRequest
+        //Mapping for RentRequestDTO to RentRequest
         modelMapper.typeMap(RentRequestDTO.class, RentRequest.class).addMappings(mapper -> {
-            mapper.skip(RentRequest::setOwner);
-            mapper.skip(RentRequest::setRenter);
-            mapper.skip(RentRequest::setProperty);
+            mapper.using(ctx -> userRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentRequestDTO::getOwnerId, RentRequest::setOwner);
+            mapper.using(ctx -> userRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentRequestDTO::getRenterId, RentRequest::setRenter);
+            mapper.using(ctx -> propertyRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentRequestDTO::getPropertyId, RentRequest::setProperty);
         });
 
-        // Mapping for PhotoDTO to Photo
-        modelMapper.typeMap(PhotoDTO.class, Photo.class).addMappings(mapper -> {
-            mapper.skip(Photo::setProperty);
+        //Mapping for RentRequest to RentRequestDTO
+        modelMapper.typeMap(RentRequest.class, RentRequestDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getOwner().getIdUser(), RentRequestDTO::setOwnerId);
+            mapper.map(src -> src.getRenter().getIdUser(), RentRequestDTO::setRenterId);
+            mapper.map(src -> src.getProperty().getIdProperty(), RentRequestDTO::setPropertyId);
         });
-        
+
+        //Mapping for RentDTO to Rent
+        modelMapper.typeMap(RentDTO.class, Rent.class).addMappings(mapper -> {
+            mapper.using(ctx -> userRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentDTO::getOwnerId, Rent::setOwner);
+            mapper.using(ctx -> userRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentDTO::getRenterId, Rent::setRenter);
+            mapper.using(ctx -> propertyRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(RentDTO::getPropertyId, Rent::setProperty);
+        });
+
+        //Mapping for Rent to RentDTO
+        modelMapper.typeMap(Rent.class, RentDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getOwner().getIdUser(), RentDTO::setOwnerId);
+            mapper.map(src -> src.getRenter().getIdUser(), RentDTO::setRenterId);
+            mapper.map(src -> src.getProperty().getIdProperty(), RentDTO::setPropertyId);
+        });
+
+        //Mapping for PhotoDTO to Photo
+        modelMapper.typeMap(PhotoDTO.class, Photo.class).addMappings(mapper -> {
+            mapper.using(ctx -> propertyRepository.findById((Long) ctx.getSource()).orElse(null))
+                .map(PhotoDTO::getPropertyId, Photo::setProperty);
+        });
+
+        //Mapping for Photo to PhotoDTO
+        modelMapper.typeMap(Photo.class, PhotoDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getProperty().getIdProperty(), PhotoDTO::setPropertyId);
+        });
+
         return modelMapper;
     }
 }
